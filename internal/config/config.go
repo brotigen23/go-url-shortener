889000da3b628b7b16fa60fca1352f6470dc4b9c
brotigen23 <125830789+brotigen23@users.noreply.github.com>
2024-10-08 23:04:47 +0300

    Revert "Добавил конфигурация с помощью переменных окружения"

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"flag"
-	"os"
 )
 
 /*
@@ -11,20 +10,14 @@ import (
 */
 
 type config struct {
-	ServerAddress string `env:"SERVER_ADDRESS"`
-	BaseURL       string `env:"BASE_URL"`
+	BaseHost           *string
+	BastHostForAliases *string
 }
 
 var Config = config{}
 
 func InitConfig() {
-	flag.StringVar(&Config.ServerAddress, "a", "localhost:8080", "base host")
-	flag.StringVar(&Config.BaseURL, "b", "http://localhost:8080", "base host for aliases")
+	Config.BaseHost = flag.String("a", "localhost:8080", "base host")
+	Config.BastHostForAliases = flag.String("b", "http://localhost:8080", "base host for aliases")
 	flag.Parse()
-	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
-		Config.ServerAddress = envRunAddr
-	}
-	if envRunAddr := os.Getenv("BASE_URL"); envRunAddr != "" {
-		Config.BaseURL = envRunAddr
-	}
 }
