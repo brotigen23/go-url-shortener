531194e6c6b45eb647c47aa7a9ca1f417bbc84f4
brotigen23 <brotigen23@gmail.com>
2024-10-31 11:19:23 +0300

    fix: use new repo implementation

--- internal/handlers/indexHandler.go
+++ internal/handlers/indexHandler.go
@@ -5,43 +5,49 @@ import (
 	"net/http"
 
 	"github.com/brotigen23/go-url-shortener/internal/config"
-	"github.com/brotigen23/go-url-shortener/internal/storage"
+	"github.com/brotigen23/go-url-shortener/internal/services"
 	"github.com/go-chi/chi/v5"
 )
 
 type indexHandler struct {
 	config  *config.Config
-	storage *storage.Storage
+	service *services.ServiceShortener
 }
 
-func NewIndexHandler(conf *config.Config, stor *storage.Storage) *indexHandler {
+func NewIndexHandler(conf *config.Config) *indexHandler {
 	return &indexHandler{
 		config:  conf,
-		storage: stor,
+		service: services.NewService(8),
 	}
 }
 
 func (handler indexHandler) HandleGET(rw http.ResponseWriter, r *http.Request) {
 	alias := chi.URLParam(r, "id")
-	url, err := handler.storage.FindByAlias([]byte(alias))
+	model, err := handler.service.GetURLByAlias(alias) //handler.repo.GetByAlias(alias)
 	if err != nil {
 		rw.WriteHeader(http.StatusNotFound)
 		return
 	}
-	rw.Header().Set("location", string(url))
+	rw.Header().Set("location", string(model.GetURL()))
 	rw.WriteHeader(http.StatusTemporaryRedirect)
 }
 
+
 func (handler indexHandler) HandlePOST(rw http.ResponseWriter, r *http.Request) {
 	url, _ := io.ReadAll(r.Body)
-	alias := handler.storage.Put(url)
+
+	model, err := handler.service.Save(string(url))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	// Заголовки и статус ответа
 	rw.Header().Set("content-type", "text/plain")
 	rw.WriteHeader(http.StatusCreated)
 
 	// Запись ответа
-	_, err := rw.Write([]byte(handler.config.BaseURL + "/" + alias))
+	_, err = rw.Write([]byte(handler.config.BaseURL + "/" + model.GetAlias()))
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusBadRequest)
 	}
