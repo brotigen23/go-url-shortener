1dc792204f5119c8942fb269d78a9bcd7c4f3b08
brotigen23 <brotigen23@gmail.com>
2024-10-31 11:19:39 +0300

    feat: add middleware log

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,15 +8,20 @@ import (
 
 	"github.com/brotigen23/go-url-shortener/internal/config"
 	"github.com/brotigen23/go-url-shortener/internal/handlers"
-	"github.com/brotigen23/go-url-shortener/internal/storage"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
-func Run(conf *config.Config, stor *storage.Storage) error {
+func Run(conf *config.Config) error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return fmt.Errorf("logger init error: %v", err)
+	}
+
 	r := chi.NewRouter()
-	indexHandler := handlers.NewIndexHandler(conf, stor)
-	r.Get("/{id}", indexHandler.HandleGET)
-	r.Post("/", indexHandler.HandlePOST)
+	indexHandler := handlers.NewIndexHandler(conf)
+	r.Get("/{id}", handlers.WithLogging(indexHandler.HandleGET, logger.Sugar()))
+	r.Post("/", handlers.WithLogging(indexHandler.HandlePOST, logger.Sugar()))
 	fmt.Printf("server is running on %v\nbase url for alias is %v\n", conf.ServerAddress, conf.BaseURL)
 	return http.ListenAndServe(conf.ServerAddress, r)
 }
