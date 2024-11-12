28ab1a7d4d940464188b36a20b9f7f27f12def98
brotigen23 <brotigen23@gmail.com>
2024-11-12 16:11:13 +0300

    fix: dont save if already exists

--- internal/repositories/postgresRepository.go
+++ internal/repositories/postgresRepository.go
@@ -48,6 +48,10 @@ func (repo *PostgresRepository) GetByAlias(alias string) (*model.Alias, error) {
 func (repo *PostgresRepository) GetByURL(url string) (*model.Alias, error) { return nil, nil }
 func (repo *PostgresRepository) GetAll() *[]model.Alias                    { return nil }
 func (repo *PostgresRepository) Save(model model.Alias) error {
+	query := repo.db.QueryRow(`SELECT * FROM Aliases WHERE URL = $1`, model.URL)
+	if query != nil {
+		return fmt.Errorf("already exist")
+	}
 	result, err := repo.db.Exec("INSERT INTO Aliases VALUES($1, $2)", model.URL, model.Alias)
 	if err != nil {
 		return err
