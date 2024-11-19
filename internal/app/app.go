6c0e71735d2b8f4b8c59192413c59070c2636855
brotigen23 <brotigen23@gmail.com>
2024-11-19 14:40:15 +0300

    fix: migrate from app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,12 +1,27 @@
 package app
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/brotigen23/go-url-shortener/internal/config"
 	"github.com/brotigen23/go-url-shortener/internal/server"
 )
 
 func Run() error {
 	config := config.NewConfig()
+	if config.DatabaseDSN != "" {
+		cmd := exec.Command("make", "migrate")
+		stdout, err := cmd.Output()
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		// Print the output
+		fmt.Println(string(stdout))
+	}
+
 	err := server.Run(config)
 
 	if err != nil {
