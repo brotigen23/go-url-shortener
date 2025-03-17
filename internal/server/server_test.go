d2e5dab4959e97d540dad10fd0b58f89f511eaa4
brotigen23 <brotigen23@gmail.com>
2025-03-17 19:52:05 +0300

    fix: codecov

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,8 +1,25 @@
 package server
 
 import (
+	"log"
+	"syscall"
 	"testing"
+
+	"github.com/brotigen23/go-url-shortener/internal/config"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
 )
 
 func TestRun(t *testing.T) {
+	logger := zap.NewNop()
+	config := &config.Config{}
+
+	go func() {
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}()
+
+	err := Run(config, logger.Sugar())
+	assert.NoError(t, err)
+
+	log.Println("DONE")
 }
